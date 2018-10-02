57b51c1cc7a6ad61547a4ae08fb7d06380ee88c2
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-02 09:19:41 -0300

    Versão inicial do projeto.

---
+++ spring/somador/src/main/java/br/edu/ifal/somador/SomadorApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifal.somador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomadorApplication.class, args);
+	}
+}
