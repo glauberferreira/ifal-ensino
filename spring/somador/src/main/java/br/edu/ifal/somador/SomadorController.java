dfed06d64969216256fee9afef7904ead62bb7f8
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-02 10:22:41 -0300

    Alterando para POST (antes GET) do método HTTP utilizado para somar.

--- spring/somador/src/main/java/br/edu/ifal/somador/SomadorController.java
+++ spring/somador/src/main/java/br/edu/ifal/somador/SomadorController.java
@@ -3,6 +3,7 @@ package br.edu.ifal.somador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SomadorController {
@@ -12,7 +13,7 @@ public class SomadorController {
 		return "formSoma";
 	}
 	
-	@GetMapping("/somar")
+	@PostMapping("/somar")
 	public String somar(Integer numero1, Integer numero2, Model model) {
 		Integer soma = numero1 + numero2;
 		
