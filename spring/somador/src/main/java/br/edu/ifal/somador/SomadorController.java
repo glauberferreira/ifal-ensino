18bd2d9a3dcbf61eb59afc38e7e69d542988a19a
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-02 09:49:52 -0300

    Criação de formulário de soma e acesso via link na página inicial.
    Neste commit, o formulário de soma ainda não está funcional.

---
+++ spring/somador/src/main/java/br/edu/ifal/somador/SomadorController.java
@@ -0,0 +1,14 @@
+package br.edu.ifal.somador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SomadorController {
+
+	@GetMapping("/formulario")
+	public String exibirForm() {
+		return "formSoma";
+	}
+	
+}
