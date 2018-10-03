c4462a92325d863d2db61461f36e992aca06d757
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-03 09:15:52 -0300

    Versão inicial do projeto com dependências Web, Devtools e Thymeleaf.

---
+++ spring/bootstrap/src/main/java/br/edu/ifal/bootstrap/BootstrapApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifal.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+}
