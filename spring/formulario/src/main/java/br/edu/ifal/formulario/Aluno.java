e6aa549a94283ecea9cb3675dcdc45a851eef317
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-04 12:29:04 -0300

    Atualizando MVC para exibir os dados do aluno enviados no formulário.

---
+++ spring/formulario/src/main/java/br/edu/ifal/formulario/Aluno.java
@@ -0,0 +1,21 @@
+package br.edu.ifal.formulario;
+
+public class Aluno {
+
+	private String nome;
+	private String matricula;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+}
---
+++ spring/formulario/src/main/java/br/edu/ifal/formulario/AlunoController.java
@@ -0,0 +1,24 @@
+package br.edu.ifal.formulario;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AlunoController {
+	
+	@GetMapping("/formulario")
+	public String exibirForm(Model model) {
+		model.addAttribute("aluno", new Aluno());
+		
+		return "formAluno";
+	}
+	
+	@PostMapping("/cadastrarAluno")
+	public String cadastrar(@ModelAttribute Aluno aluno) {		
+		return "exibirAluno";
+	}
+	
+}
