e710e8816bbe70708d237e6eceab0cabdce1c8b1
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-04 12:06:15 -0300

    Versão inicial do projeto após criação usando https://start.spring.io/

---
+++ spring/formulario/src/main/java/br/edu/ifal/formulario/FormularioApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifal.formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioApplication.class, args);
+	}
+}
