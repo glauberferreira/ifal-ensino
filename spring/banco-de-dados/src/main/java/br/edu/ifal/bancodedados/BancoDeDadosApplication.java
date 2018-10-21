9a743bc707960f15b9c87b00a7d94cb15f24c1bd
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-21 17:54:35 -0300

    Versão inicial do projeto após criação usando https://start.spring.io/
    * Web
    * Devtools
    * Thymeleaf

---
+++ spring/banco-de-dados/src/main/java/br/edu/ifal/bancodedados/BancoDeDadosApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifal.bancodedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeDadosApplication.class, args);
+	}
+}
