1240cccb73a7e899b0159269f3092fe217e4b261
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-10-21 18:00:44 -0300

    Adicionando MVC para exibir os dados do aluno enviados no formulário.

---
+++ spring/banco-de-dados/src/main/java/br/edu/ifal/bancodedados/Aluno.java
@@ -0,0 +1,24 @@
+package br.edu.ifal.bancodedados;
+
+public class Aluno {
+
+	private String nome;
+	private String matricula;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+}
