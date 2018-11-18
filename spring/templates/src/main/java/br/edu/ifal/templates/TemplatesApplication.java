f92d3f600c5f4ff8b35e9298b452fc458c5b92e1
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2018-11-18 10:08:42 -0300

    Versão inicial do projeto templates

---
+++ spring/templates/src/main/java/br/edu/ifal/templates/TemplatesApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifal.templates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatesApplication.class, args);
+	}
+}
